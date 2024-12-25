68e02383b8985ff611d77a3e2458b5d08c19b5e4
Balaji Sankarapu <balajisankarapu@Balajis-MacBook-Pro.local>
2024-12-25 15:54:26 -0600

    aod-server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const connection = require('./db');
 const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
+const serverless = require('serverless-http');
 
 //submarine database connection calling
 connection()
@@ -17,5 +18,5 @@ app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 
 const port = process.env.PORT || 8080;
-app.listen(port, () =>  console.log(`server is running on ${port}`))
-
+app.listen(port, () =>  console.log(`server is running on ${port}`));
+module.exports.handler = serverless(app);
